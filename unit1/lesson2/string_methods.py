f6023165169a247ffa66179df302c9bc31090234
NYCDOE <admin@AC02KG05KF67G.local>
2017-09-19 08:03:47 -0400

    Added lesson 2 files

---
+++ unit1/lesson2/string_methods.py
@@ -0,0 +1,7 @@
+text = 'tHis iS HoW a tWO yEaR olD tyPEs.'
+
+print('Upper: ' + text.upper())
+print('Lower: ' + text.lower())
+print('Title Case: ' + text.title())
+print('Capitalize: ' + text.capitalize())
+print('Swapcase: ' + text.swapcase())
