4fd94b21f4630074a2f7c1bb53bd0763cce3886e
Cary Riina <cary.e.riina@gmail.com>
2017-11-08 09:12:58 -0500

    Editted RPS for styling.

--- unit3/lesson2/rockpaperscissorsBasic.py
+++ unit3/lesson2/rockpaperscissorsBasic.py
@@ -7,11 +7,11 @@ print("Let's play!")
 playerChoice = input('Rock, Paper, Scissors shoot: ')
 playerChoice = playerChoice.title()
 computerChoice = random.choice(['Rock','Paper','Scissors'])
-print('\t\t\tPlayer chooses: {}\n\t\t\t\t vs \n\t\t\tComputer chooses: {}'.format(playerChoice,computerChoice))
+print('\n\t\t\t****************************\n\t\t\tPlayer chooses: {}\n\t\t\t\t vs \n\t\t\tComputer chooses: {}\n\t\t\t****************************\n'.format(playerChoice,computerChoice))
 if playerChoice == computerChoice:
 	print("It's a tie!")
 elif (playerChoice == 'Rock' and computerChoice == 'Scissors') or (playerChoice == 'Scissors' and computerChoice == 'Paper') or (playerChoice == 'Paper' and computerChoice == 'Rock'):
-	print('Player Wins')
+	print('Player WINS!')
 else:
-	print('Player Loses')
+	print('Player LOSES!')
 print('-'*67)
